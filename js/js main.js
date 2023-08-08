1fb339c9307652ab34a713e83b4e4573100ac63d
iampeterjohn <peterjeche442@gmail.com>
2023-08-08 02:40:44 -0700

    hiring form proj

---
+++ js/js main.js
@@ -0,0 +1,43 @@
+// Array lesson 
+/* array is use to hold multiple values in javascript,with array you will be able to loop through the values provided,array can span multiple lines aslo space isn't a problem in an array
+
+*/
+// an empty array creadit with empty literals
+// const cars = ["mafia car", "bmw", "fiat", "toyota" ]
+// cars[cars.length] = "been"
+
+// array methods include
+// toString,join,flat,concat,pop,push,sort,revserse,delete,unshift,shift,slice,splice,array length
+// cars.toString()
+// cars.pop()
+// cars.push("grab");
+// cars.shift();
+// cars.unshift("claw");
+// cars.sort();
+// cars.reverse();
+// cars.concat("mimi")
+// cars.splice(0,0, "mimimo", "gloria");
+// cars.slice()
+// console.log(cars.slice(1))
+
+// const carss = [
+//   {type:"Volvo", year:2016},
+//   {type:"Saab", year:2001},
+//   {type:"BMW", year:2010}
+// ];
+// const myArray = [["fiat"],["toyota"],["bmw"]];
+// console.log(myArray.flat());
+
+const myArray = [0, "great", 20, 50, 3, 5, 9, 1];
+let text = myArray.constructor
+// let text = myArray.reduce(myFunction);
+// function myFunction( total, value, index, array) {
+//   return total + value 
+  
+// }
+console.log(text)
+
+// myArray.sort( function( a, b ){ return 0.5 - Math.random() + 1})
+// console.log(myArray)
+
+// array iteration 
